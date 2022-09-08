7a50cafeebffcffcaf32cbae6b57e01246616989
rocio2504 <rocioandrea250489@gmail.com>
2022-09-07 20:21:08 -0500

    Update app component html & app component ts (class 6)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Rocío'
+  city = 'Callao'
+  img =
+    'https://cdn.pixabay.com/photo/2017/10/10/07/48/hills-2836301_960_720.jpg'
 }
